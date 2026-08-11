96bfc599d2344a92e93823c8878826cf605a95d1
Alexis Estadistica <choche_.28@hotmail.com>
2026-08-11 13:54:36 -0600

    update

---
+++ scripts/Primer_script_Proyecto.R
@@ -0,0 +1,14 @@
+# Jorge Alexis Luna Robles
+# 2134500
+# 11/08/2026
+
+# Ingresar Datos -------------
+
+altura <- 12
+12 + altura
+altura * altura 
+altura + dbh
+# Conjunto de datos 
+
+dbh <- c(12, 10, 11.5, 13.2, 18.5)
+# Estimar medias ----------- 

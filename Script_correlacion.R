c1db8e83e55681322db2f7edaca2c84db0ff776f
Alexis Estadistica <choche_.28@hotmail.com>
2026-09-23 09:40:42 -0600

    update

--- Script_correlacion.R
+++ Script_correlacion.R
@@ -31,3 +31,48 @@ hist(datos$dbh_sqr)
 
 cor.test(datos$diametro, datos$altura, method = "pearson")
 
+# Datos de Geiser
+
+# correlación si te esperas 10 minutos habrá alguna erupción
+# Tiempo entre una erupcion y otra y su duración
+
+# Faithful geyser
+
+geyser <- faithful
+View(faithful)
+
+fivenum(geyser$eruptions)
+fivenum(geyser$waiting)
+mean(geyser$eruptions)
+mean(geyser$waiting)
+
+plot(geyser$waiting, geyser$eruptions,
+     pch = 19, col = "red",
+     xlab = "Tiempo de espera (min)",
+     ylab = "Erupción (min)")
+
+cor.test(geyser$eruptions, geyser$waiting)
+cor.test(geyser$waiting, geyser$eruptions)
+
+g.lm <- lm(geyser$eruptions ~ geyser$waiting)
+summary(g.lm)
+
+plot(geyser$waiting, geyser$eruptions,
+     pch = 19, col = "lightgreen",
+     xlab = "Tiempo de espera (min)",
+     ylab = "Erupción (min)")
+abline(g.lm, col = "red",
+       lwd = 2)
+g.lm$coefficients
+g.lm$residuals
+
+anova(g.lm)
+
+geyser$yp <- g.lm$fitted.values
+geyser$res <- g.lm$residuals
+sum(geyser$res)
+
+geyser$modelo <- -1.874016 + 0.07563*geyser$waiting
+
+tiem <- c(45, 55, 68)
+-1.874016 + 0.07563*tiem
